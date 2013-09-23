b632ba4beb864de995f28bd59adad715db00d6e0
Mako <jlauve@rsmw.net>
2013-09-22 23:34:06 -0400

    Store color palette in world state

--- Main.hs
+++ Main.hs
@@ -2,13 +2,14 @@ import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 
 import Control.Arrow ((***))
+import Data.List (unfoldr)
 
 import State
 
 main :: IO ()
 main = play window black fps world frame moveCursor pass
   where fps = 30
-        world = Editor (0, 0) 8 8
+        world = Editor (0, 0) 8 8 allColors doubleRainbow
 
 drawCursor :: Editor -> Picture
 drawCursor world = inPlace $ color white $ thickCircle (size / 4) 2
@@ -34,6 +35,23 @@ moveCursor (EventKey (Char c) Down _ _) world =
           0 <= y && y < height world
 moveCursor _ world = world
 
+doubleRainbow :: Palette
+doubleRainbow = quadInterp 8 magenta cyan red green
+
+quadInterp :: Int -> Color -> Color -> Color -> Color -> Palette
+quadInterp n bl br tl tr = concat $ zipWith mix (mix tl bl) (mix tr br)
+  where high = fromIntegral $ n - 1
+        ratios = map (/ high) [0 .. high]
+        mix l r = do
+          v <- ratios
+          let u = 1 - v
+          return $ mixColors u v l r
+
+allColors :: TileMap
+allColors = unfoldr go [0 .. 8 * 8 - 1]
+  where go [] = Nothing
+        go xs = Just $ splitAt 8 xs
+
 pass :: a -> b -> b
 pass = flip const
 
@@ -46,13 +64,11 @@ frame world = Pictures $ tiles world ++ [drawCursor world]
 tiles :: Editor -> [Picture]
 tiles world = [ go x y | x <- [0 .. pred w], y <- [0 .. pred h] ]
   where go x y =
-          Color (makeColor (down w x) (up w x) (up h y) 1) $
+          Color (palette world !! (x + w * y)) $
           onPixel (x, y) world $
           (rectangleSolid size size)
         w = width world
         h = height world
-        up k i = fromIntegral i / fromIntegral k
-        down k i = fromIntegral (k - i) / fromIntegral k
 
 onPixel :: (Int, Int) -> Editor -> Picture -> Picture
 onPixel (x, y) world = Translate (size * bigx) (size * bigy)
--- State.hs
+++ State.hs
@@ -1,7 +1,15 @@
 module State where
 
+import Graphics.Gloss.Data.Color
+
+type Palette = [Color]
+
+type TileMap = [[Int]]
+
 data Editor = Editor
   { cursor :: (Int, Int)
   , width :: Int
   , height :: Int
+  , pixels :: TileMap
+  , palette :: Palette
   }
